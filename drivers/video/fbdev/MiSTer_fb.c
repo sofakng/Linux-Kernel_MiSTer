04d779d0d4ae20734ea66a2e6e9f7964882be748
Sorgelig <pour.garbage@gmail.com>
2019-05-25 03:34:39 +0800

    MiSTer_fb: implement FBIO_WAITFORVSYNC.

--- drivers/video/fbdev/MiSTer_fb.c
+++ drivers/video/fbdev/MiSTer_fb.c
@@ -18,26 +18,40 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
+#include <linux/interrupt.h>
+#include <linux/of_irq.h>
+#include <linux/uaccess.h>
 
-#define PALETTE_SIZE	256
-#define DRIVER_NAME	    "MiSTer_fb"
+#define PALETTE_SIZE       256
+#define DRIVER_NAME        "MiSTer_fb"
+#define VSYNC_TIMEOUT_MSEC 50
 
-static u32 width = 0, height = 0, stride = 0, format = 8888, rb = 1;
+static u32 width = 0, height = 0, stride = 0, format = 8888, rb = 1, frame_count = 0;
 module_param(width,  uint, 0444);
 module_param(height, uint, 0444);
 module_param(stride, uint, 0444);
 module_param(format, uint, 0444);
 module_param(rb,     uint, 0444);
+module_param(frame_count, uint, 0444);
 
 struct fb_dev {
 	struct platform_device *pdev;
 	struct fb_info info;
 	struct resource *fb_res;
 	void __iomem *fb_base;
+	int irq;
 	u32 pseudo_palette[PALETTE_SIZE];
 };
 
-static int setcolreg(unsigned regno, unsigned red, unsigned green,
+static wait_queue_head_t vs_wait;
+static irqreturn_t irq_handler(int irq, void *par)
+{
+	frame_count++;
+	wake_up_interruptible(&vs_wait);
+	return IRQ_HANDLED;
+}
+
+static int fb_setcolreg(unsigned regno, unsigned red, unsigned green,
 			   unsigned blue, unsigned transp, struct fb_info *info)
 {
 	/*
@@ -61,12 +75,55 @@ static int setcolreg(unsigned regno, unsigned red, unsigned green,
 	return 0;
 }
 
+static int fb_wait_for_vsync(u32 arg)
+{
+	u32 count;
+	int ret;
+
+	if (arg != 0) return -ENODEV;
+
+	count = frame_count;
+	ret = wait_event_interruptible_timeout(vs_wait,
+				       count != frame_count,
+				       msecs_to_jiffies(VSYNC_TIMEOUT_MSEC));
+
+	if (ret == 0)
+		return -ETIMEDOUT;
+
+	return 0;
+}
+
+static int fb_ioctl(struct fb_info *info, unsigned int cmd, unsigned long arg)
+{
+	int ret;
+	u32 ioarg;
+
+	switch (cmd)
+	{
+	case FBIO_WAITFORVSYNC:
+		if (get_user(ioarg, (u32 __user *)arg))
+		{
+			ret = -EFAULT;
+			break;
+		}
+
+		ret = fb_wait_for_vsync(ioarg);
+		break;
+	default:
+		ret = -ENOTTY;
+	}
+
+	return ret;
+}
+
+
 static struct fb_ops ops = {
 	.owner = THIS_MODULE,
 	.fb_fillrect = cfb_fillrect,
 	.fb_copyarea = cfb_copyarea,
 	.fb_imageblit = cfb_imageblit,
-	.fb_setcolreg = setcolreg,
+	.fb_setcolreg = fb_setcolreg,
+	.fb_ioctl = fb_ioctl,
 };
 
 static int setup_fb_info(struct fb_dev *fbdev)
@@ -165,7 +222,7 @@ static int fb_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "devm_ioremap_resource fb failed\n");
 		return PTR_ERR(fbdev->fb_base);
 	}
-	
+
 	retval = setup_fb_info(fbdev);
 	fbdev->info.fix.smem_start = fbdev->fb_res->start;
 	fbdev->info.screen_base = fbdev->fb_base;
@@ -183,6 +240,16 @@ static int fb_probe(struct platform_device *pdev)
 		 (unsigned)fbdev->info.fix.smem_start,
 		 fbdev->info.fix.smem_len, fbdev->fb_base);
 
+	fbdev->irq = irq_of_parse_and_map(pdev->dev.of_node, 0);
+	dev_info(&pdev->dev, "fb%d: IRQ=%d\n", fbdev->info.node, fbdev->irq);
+
+	init_waitqueue_head(&vs_wait);
+
+	if (fbdev->irq == NO_IRQ || request_irq(fbdev->irq, &irq_handler, IRQF_SHARED, "MiSTer_fb", fbdev))
+	{
+		dev_err(&pdev->dev, "request_irq failed. fb won't use IRQ.\n");
+	}
+
 	p_fbdev = fbdev;
 	return 0;
 
@@ -199,6 +266,7 @@ static int fb_remove(struct platform_device *dev)
 	{
 		unregister_framebuffer(&fbdev->info);
 		fb_dealloc_cmap(&fbdev->info.cmap);
+		if(fbdev->irq != NO_IRQ) free_irq(fbdev->irq, dev);
 	}
 	return 0;
 }
